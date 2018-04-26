5eb7b78b28ce3f6974b7bfd76bee50267510e4d9
Skid <skid@tuto-craft.com>
2018-04-26 21:20:16 +0200

    Selfhost assets

--- query/status.php
+++ query/status.php
@@ -24,15 +24,15 @@ This file is part of Minecraft-RCON-Console.
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Minecraft RCON Console</title>
 
-	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script src="assets/js/jquery-1.11.0.min.js"></script>
+	<script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
 
 	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap/3.1.1/bootstrap.min.css">
 	<!-- Optional theme -->
-	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap/3.1.1/bootstrap-theme.min.css">
 	<!-- Latest compiled and minified JavaScript -->
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<script src="assets/js/bootstrap/3.1.1/bootstrap.min.js"></script>
 	<script type="text/javascript" src="../script.js"></script>
 
 	<meta http-equiv="refresh" content="30">
